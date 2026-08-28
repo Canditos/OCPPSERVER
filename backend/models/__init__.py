2ea6ab1b006c73ef31f08520c4779cdaff41a2f6
Marco Candido <marco.candido@gmail.com>
2026-08-28 15:38:34 +0100

    fix: correct models import in __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,13 +1,14 @@
-from models.charger import Charger, Connector, OcppMessage, AvailabilityLog, ChargerCertificate
+from models.charger import Charger, Connector, OcppMessage, AvailabilityLog
 from models.transaction import Transaction, MeterValue
 from models.configuration import ChargerConfiguration
 from models.auth_token import AuthToken
 from models.authorized_tag import AuthorizedTag
 from models.charging_profile import ChargingProfile, ChargingProfileModel
 from models.user import User
+from models.meter_public_key import MeterPublicKey
 
 __all__ = [
-    "Charger", "Connector", "OcppMessage", "AvailabilityLog", "ChargerCertificate",
+    "Charger", "Connector", "OcppMessage", "AvailabilityLog",
     "Transaction", "MeterValue",
     "ChargerConfiguration",
     "AuthToken",
@@ -15,6 +16,5 @@ __all__ = [
     "ChargingProfile",
     "ChargingProfileModel",
     "User",
+    "MeterPublicKey",
 ]
-
-from models.meter_key import MeterPublicKey
